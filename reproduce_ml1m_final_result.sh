504ff598778ee493829d32979770179e134c04ac
k9luo <lucas.luokai@gmail.com>
2019-06-05 15:00:03 -0400

    uncomment

--- reproduce_ml1m_final_result.sh
+++ reproduce_ml1m_final_result.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#python3 main.py --path data/ --active_model Greedy --active_iteration 50
+python3 main.py --path data/ --active_model Greedy --active_iteration 50
 python3 main.py --path data/ --active_model Greedy --active_iteration 50 --disable_sample_from_all
 python3 main.py --path data/ --active_model Greedy --active_iteration 50 --disable_iterative
 python3 main.py --path data/ --active_model Greedy --active_iteration 50 --disable_iterative --disable_sample_from_all
--- reproduce_yelp_final_result.sh
+++ reproduce_yelp_final_result.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#python3 main.py --path data/ --active_model Greedy --epoch 300 --lamb 0.001 --rank 200 --active_iteration 50
+python3 main.py --path data/ --active_model Greedy --epoch 300 --lamb 0.001 --rank 200 --active_iteration 50
 python3 main.py --path data/ --active_model Greedy --epoch 300 --lamb 0.001 --rank 200 --active_iteration 50 --disable_sample_from_all
 python3 main.py --path data/ --active_model Greedy --epoch 300 --lamb 0.001 --rank 200 --active_iteration 50 --disable_iterative
 python3 main.py --path data/ --active_model Greedy --epoch 300 --lamb 0.001 --rank 200 --active_iteration 50 --disable_iterative --disable_sample_from_all
